c9d33a5093c44091fdb168e937840afd27b08947
Andii-Ok <andrii_23@icloud.com>
2024-06-02 12:22:51 +0300

    Andrii-Ok

--- Andrii.py
+++ Andrii.py
@@ -1,12 +1,15 @@
-digit = int(input('введите 5-х значное число:'))
+z = input("Действие (+,*,- или /);")
+x = float(input('x = '))
+y = float(input('y = '))
+if z == '+':
+     print(x + y)
+elif z == '-':
+     print(x - y)
+elif z == '*':
+     print(x * y)
+elif z == '/':
+    if y != 0:
+     print(x / y)
+    else:
+     print('деление на ноль!')
 
-a, b = divmod(digit,10000)
-a1, b1 = divmod(b,1000)
-a2, b2 = divmod(b1,100)
-a3, b3 = divmod(b2,10)
-
-cut1 = (b3*10000+a3*1000+a2*100+a1*10+a*1)
-
-
-
-print(cut1)
